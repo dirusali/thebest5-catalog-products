0654ec88d83f30c61b2676b5301c4b169af6d7bc
mariano <marianomartinez84@gmail.com>
2017-09-26 12:45:00 -0300

    I set pagination to 10

--- thebest5_catalog_products/settings.py
+++ thebest5_catalog_products/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
