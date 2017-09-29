72e0363b989df7be7a4d77a915833e01e517dfa1
Mario Moya <mmoya@8ksoft.com.ar>
2017-09-29 11:29:53 -0300

    Fix problem with search terms of only one word

--- products/filters.py
+++ products/filters.py
@@ -2,15 +2,22 @@ from rest_framework.filters import SearchFilter
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 class FullTextSearchFilter(SearchFilter):
+    """ 
+    Class that implements the filter_queryset method to use PostgreSQL Full text search
+    See https://docs.djangoproject.com/en/1.11/ref/contrib/postgres/search/
+    """
     
     def filter_queryset(self, request, queryset, view):
         search_fields = getattr(view, 'search_fields', None)
         params = request.query_params.get(self.search_param, '')
         search_terms = self.get_search_terms(request)
+        if len(search_terms) < 2:
+            # if there are only one search term return the super implementation without full text search
+            return super(FullTextSearchFilter, self).filter_queryset(request, queryset, view)
 
         if not search_fields or not search_terms:
             return queryset
-        
+            
         extra_weight = ['B','C','D']
         vector = SearchVector(search_fields[0], weight='A')
         for i,search_field in enumerate(search_fields[1:]):
