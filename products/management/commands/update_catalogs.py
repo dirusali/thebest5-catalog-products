2698d7b7d5c4273493fc799c169a82028cefee5d
mariano <marianomartinez84@gmail.com>
2017-10-05 15:38:56 -0300

    Changes in model to automatize catalog updates

---
+++ products/management/commands/update_catalogs.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.aggregates import StringAgg
+
+from products.models import Product
+
+
+class Command(BaseCommand):
+    help = 'Import a csv into `Product` database.'
+
+    def handle(self, *args, **options):
+        print("Updating search vector for full text search..")
+        vector = SearchVector('name', weight='A') + SearchVector('description', weight='B') + SearchVector('brand', weight='C')
+        Product.objects.update(search_vector=vector)
+        print("Update complete.")
+
