fd9f33f0f4314b74a52dc832bee38e51fc09e73a
Diego <dirusali@gmail.com>
2018-05-09 20:31:46 +0200

    Adding scripts to update catalogs
    Toupdate: takes the feed of awin and returns which catalogs need to be updated today
    Update_today: does the same + executing the update of these catalogs

---
+++ products/management/commands/toupdate.py
@@ -0,0 +1,39 @@
+#from dateutil import parser
+import csv
+import requests
+from datetime import date
+from products.models import AutomaticProductUpdate
+from django.core.management.base import BaseCommand
+
+today = date.today()
+url = 'https://productdata.awin.com/datafeed/list/apikey/8ea641145a26285b745ac80a2031b7e1'
+
+class Command(BaseCommand):
+    help = "Returns the catalogs which need to be updated today"
+    def handle(self, *args, **options):
+        update_awin_list = AutomaticProductUpdate.objects.filter(enabled=False)
+        update_rest_list = AutomaticProductUpdate.objects.filter(enabled=True)
+        with requests.Session() as f:
+            download = f.get(url)
+            decoded = download.content.decode('utf-8')
+            feed = csv.reader(decoded.splitlines(), delimiter=',')
+            updates = list(feed)
+            for row in updates:
+                shop_name = row[1]
+                last_update = row[8]
+                for conf in update_awin_list:
+                    shop = conf.shop
+                    if shop == shop_name:
+                        if str(today) == last_update[0:10]:
+                            conf.update_today = True
+                        else:
+                            conf.update_today = False
+
+            for conf in update_rest_list:
+                last_update = conf.last_update
+                if today == last_update:
+                    conf.update_today = False
+                else:
+                    conf.update_today = True
+
+
--- products/management/commands/update_today.py
+++ products/management/commands/update_today.py
@@ -1,6 +1,10 @@
+import csv
 import os
 import shutil
+from dateutil import parser
 import zipfile
+import requests
+from datetime import date
 from datetime import datetime
 from urllib.request import URLopener
 from django.core.management.base import BaseCommand
@@ -8,6 +12,34 @@ from products.management.commands.importcsv import load_catalog_to_db
 from products.management.commands.update_search_vector import update_search_vector
 from products.models import AutomaticProductUpdate
 from thebest5_catalog_products.settings import CATALOGS_ROOT
+
+today = date.today()
+url = 'https://productdata.awin.com/datafeed/list/apikey/8ea641145a26285b745ac80a2031b7e1'
+awin = AutomaticProductUpdate.objects.filter(enabled=False)
+rest = AutomaticProductUpdate.objects.filter(enabled=True)
+lista = list()
+
+with requests.Session() as f:
+    download = f.get(url)
+    decoded = download.content.decode('utf-8')
+    feed = csv.reader(decoded.splitlines(), delimiter=',')
+    updates = list(feed)
+    for row in updates:
+        shop_name = row[1]
+        last_update = row[8]
+        for conf in awin:
+            if shop_name == conf.shop:
+                if str(today) == last_update[0:10]:
+                    lista.append(conf)
+                else:
+                    print ("no updates pending in awin")
+
+for conf in rest:
+    last_update = conf.last_update
+    if today == last_update:
+        pass
+    else:
+        lista.append(conf)
 
 
 def decompress_file(input_file, output_dir, compression_format):
@@ -23,25 +55,24 @@ class Command(BaseCommand):
     help = "Downloads the last catalogs for each shop and updates 'Product' database."
     def handle(self, *args, **options):
         print("Updating catalogs..")
-        update_conf_list = AutomaticProductUpdate.objects.filter(update_today=True)
-        for conf in update_conf_list:
-            shop_name = conf.shop
+        for catalogo in lista:
+            shop_name = catalogo.shop
             print("Updating catalog for shop '%s'.." % shop_name)
             print("-------------------------------------------------------- ")
             try:
-                print("Dowloading catalog file for shop '%s', from url:%s" % (shop_name, conf.catalog_url))
+                print("Dowloading catalog file for shop '%s', from url:%s" % (shop_name, catalogo.catalog_url))
                 file = URLopener()
                 if not os.path.exists(CATALOGS_ROOT):
                     os.makedirs(CATALOGS_ROOT)
                 catalog_filename = CATALOGS_ROOT+'/%s_catalog' % shop_name
-                if conf.is_compressed:
-                    extension = '.%s' % conf.compress_format
+                if catalogo.is_compressed:
+                    extension = '.%s' % catalogo.compress_format
                 else:
                     extension = '.csv'
                 catalog_filename += extension
-                file.retrieve(conf.catalog_url, catalog_filename)
+                file.retrieve(catalogo.catalog_url, catalog_filename)
                 print("Catalog file retrieved for shop '%s', local path:%s" % (shop_name, catalog_filename))
-                if conf.is_compressed:
+                if catalogo.is_compressed:
                     print("Decompressing file ...")
                     # Get a new clean tmp dir
                     tmp_dir = CATALOGS_ROOT + '/%s_tmp' % shop_name
@@ -51,7 +82,7 @@ class Command(BaseCommand):
                     # Extract catalog (should be a single file inside compressed file)
                     if not decompress_file(input_file=catalog_filename,
                                            output_dir=tmp_dir,
-                                           compression_format=conf.compress_format):
+                                           compression_format=catalogo.compress_format):
                         print("Decompressing file ... ERROR")
                         return -1
                     # Copy and rename the extracted catalog file
@@ -61,20 +92,21 @@ class Command(BaseCommand):
                     shutil.copyfile(extracted_catalog_path, catalog_filename)
                     print("Decompressing file ... DONE")
                 print("Import products from file to DB ...")
-                records_num = load_catalog_to_db(shop=conf.shop,
+                records_num = load_catalog_to_db(shop=catalogo.shop,
                                    catalog_path=catalog_filename,
-                                   delimiter=conf.delimiter,
+                                   delimiter=catalogo.delimiter,
                                    delete_products=True,
                                    print_errors=False)
-                conf.last_update = datetime.now()
-                conf.local_file = catalog_filename
-                conf.records_num = records_num
-                conf.save()
+                catalogo.last_update = datetime.now()
+                catalogo.local_file = catalog_filename
+                catalogo.records_num = records_num
+                catalogo.save()
                 print("Import products from file to DB ... DONE")
             except Exception as e:
                 print("ERROR Importing updating catalog for shop %s [SKIPPED]\n%s" %(shop_name, e))
-                continue
+        else:
             print("-------------------------------------------------------- ")
+
         print("All catalogs processed.")
         update_search_vector()
         print("Catalogs update complete.")
